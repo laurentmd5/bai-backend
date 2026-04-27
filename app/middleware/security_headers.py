00f465cc920b9acc969a6000da90beefee51e73d
laurentmd5 <mavoungoumercialaurent@gmail.com>
2026-04-27 14:19:25 +0000

    update security_headers

--- app/middleware/security_headers.py
+++ app/middleware/security_headers.py
@@ -115,7 +115,9 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response.headers["Cross-Origin-Resource-Policy"] = "same-origin"
         
         # Remove server fingerprinting headers
-        response.headers.pop("Server", None)
-        response.headers.pop("X-Powered-By", None)
+        if "Server" in response.headers:
+            del response.headers["Server"]
+        if "X-Powered-By" in response.headers:
+            del response.headers["X-Powered-By"]
         
         return response
